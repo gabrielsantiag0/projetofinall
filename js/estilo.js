d604a26e1d1bc40d9a19a9fa8a360b333b32d2db
gabrielsantiag0 <gabrielleandrosantiag09@gmail.com>
2023-03-06 17:25:43 -0300

    falta pouco

--- js/estilo.js
+++ js/estilo.js
@@ -1,3 +1,5 @@
+    //avaliação inicio
+
 const estrelas = document.querySelectorAll('.estrela');
 
 estrelas.forEach(estrela => {
@@ -12,3 +14,29 @@ estrelas.forEach(estrela => {
     });
   });
 });
+
+    //avaliação final//
+
+  // favorito inicio\\
+
+var favoriteBtn = document.getElementById('favorite-btn');
+var favoriteIcon = document.getElementById('favorite-icon');
+
+favoriteBtn.addEventListener('click', function() {
+  if (favoriteIcon.classList.contains('fa-heart-o')) {
+    favoriteIcon.classList.remove('fa-heart-o');
+    favoriteIcon.classList.add('fa-heart');
+    favoriteIcon.style.color = '#ff0000';
+    favoriteBtn.innerHTML = 'Favorito ✓';
+  } else {
+    favoriteIcon.classList.remove('fa-heart');
+    favoriteIcon.classList.add('fa-heart-o');
+    favoriteIcon.style.color = '';
+    favoriteBtn.innerHTML = 'Favorito';
+  }
+});
+    //favorito final\\
+   
+    function adicionarAoCarrinho() {
+			// Adiciona o item ao carrinho de compras
+			alert("Produto adicionado ao carrinho!");}
